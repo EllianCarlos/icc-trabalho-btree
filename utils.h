3e54f20e0b8d58358a8d8cf2feca38a3c3a257db
elliancarlos <17656878+EllianCarlos@users.noreply.github.com>
2021-06-21 22:21:26 -0300

    declara funções da util no header

--- utils.h
+++ utils.h
@@ -1,8 +1,21 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
+#define OP_INSERT 1
+#define OP_UPDATE 2
+#define OP_SEARCH 3
+#define OP_EXIT 4
+
 typedef struct Errors {
     unsigned int* errorCode;
 } Errors;
+
+int getOperation();
+void startToken();
+int getNumberFromToken();
+char* getNameFromToken();
+char* getCourseFromToken();
+double getGradeFromToken();
+void exit();
 
 #endif // UTILS_H_
