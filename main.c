a1b437fa62b91fdd840f87969601b36231fbb314
elliancarlos <17656878+EllianCarlos@users.noreply.github.com>
2021-06-29 00:41:35 -0300

    feat: adiciona controle principal da main

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "btree.h"
 #include "utils.h"
+#include "datafile.h"
 #include "student.h"
 
 #include <stdio.h>
@@ -7,20 +8,78 @@
 
 #define MAX_LINE_SIZE 256
 #define MAX_WORD_SIZE 64
+#define datafilename "datafile.bin"
+#define btreefilename "btree.bin"
+
+void closeAndFreeAll(FILE *bTreeFile, btPage *pageToDeallocate)
+{
+    fclose(bTreeFile);
+    deallocatePage(pageToDeallocate);
+}
+
+void throwErrorAndClose(const char *ErrorMessage, FILE *bTreeFile, btPage *pageToDeallocate)
+{
+    fprintf(stderr, ErrorMessage);
+    closeAndFreeAll(bTreeFile, pageToDeallocate);
+    exitSystem();
+}
 
 int main()
 {
+    FILE *bTreeFile = fopen(btreefilename, "ab+");
+    btPage *bTreePage = getOrCreateRoot(bTreeFile);
+
     char line[MAX_LINE_SIZE];
     fgets(line, sizeof(line), stdin);
 
     int opCode = getOperation(line);
 
-    if (opCode == OP_INSERT)
+    if (OP_INSERT == opCode)
+    {
+
+        student *st = getStudentFromLine(line);
+        long RNN = appendAsFixedSize(datafilename, st);
+        record *newRecord = createRecord(st->nUsp, RNN);
+        bool opResult = bTreeInsert(newRecord, bTreePage, bTreeFile);
+        deleteStudent(st);
+        if (opResult == false)
+        {
+            throwErrorAndClose("Erro ao adicionar registro", bTreeFile, bTreePage);
+        }
+    }
+    else if (OP_UPDATE == opCode)
     {
         student *st = getStudentFromLine(line);
-        printStudent(st);
+        long RNN = bTreeSelect(bTreePage, st->nUsp, bTreeFile);
+        if (RNN == -1)
+        {
+            fprint(stdout, "Registro nao encontrado!");
+        }
+        else
+        {
+            updateStudent(RNN, datafilename, st);
+        }
         deleteStudent(st);
     }
-
+    else if (OP_SEARCH == opCode)
+    {
+        int nUsp = getNUspFromLine(line);
+        long RNN = bTreeSelect(bTreePage, nUsp, bTreeFile);
+        if (RNN == -1)
+        {
+            fprint(stdout, "Registro nao encontrado!");
+        }
+        else
+        {
+            student st;
+            getByRNN(RNN, datafilename, &st);
+            printStudent(&st);
+        }
+    }
+    else
+    {
+        closeAndFreeAll(bTreeFile, bTreePage);
+        exitSystem();
+    }
     return 0;
 }
