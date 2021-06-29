7ca864e61f9a5cdf0bd1ebf4604f539531345a76
elliancarlos <17656878+EllianCarlos@users.noreply.github.com>
2021-06-28 23:27:09 -0300

    feat: adiciona código de manipulação de arquivo de dados

--- dataFile.c
+++ dataFile.c
@@ -1,4 +1,5 @@
-#include "readFromFixedSizeFile.h"
+#include "dataFile.h"
+
 
 void readEverythingFromFile(FILE * filePointer, int numberOfStudentsInFile) {
     rewind(filePointer);
@@ -23,4 +24,36 @@ void readFileFromRange(FILE *filePointer, int offset, int numberOfReads) {
     fread(st, sizeof(student), numberOfReads, filePointer);
     printStudentArray(st, numberOfReads);
     free(st);
+}
+
+
+long appendAsFixedSize(char *fileName, student s)
+{
+    FILE *filePointer = fopen(fileName, "a+");
+    fseek(filePointer, 0, SEEK_END);
+    long offset = ftell(filePointer);
+    fwrite(&s, sizeof(student), 1, filePointer);
+    fflush(filePointer);
+    fclose(filePointer);
+    return offset;
+}
+
+void getByRNN(long RNN, char *filename, student *st, int nUsp)
+{
+    FILE *studentfile = fopen(filename, "ab+");
+    rewind(studentfile);
+    fseek(studentfile, RNN, SEEK_CUR);
+    fread(st, sizeof(student), 1, studentfile);
+    fclose(studentfile);
+    return;
+}
+
+void updateStudent(long RNN, char *filename, student *st)
+{
+    FILE *studentFile = fopen(filename, "w+");
+    rewind(studentFile);
+    fseek(studentFile, RNN, SEEK_CUR);
+    fwrite(st, sizeof(student), 1, studentFile);
+    fclose(studentFile);
+    return;
 }
---
+++ dataFile.h
@@ -0,0 +1,15 @@
+#ifndef DATAFILE_H_
+#define DATAFILE_H_ 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "student.h"
+
+long appendAsFixedSize(char *, student s);
+void getByRNN(long, char *, student *, int);
+void readEverythingFromFile(FILE*,int);
+int getNumberOfStudentsInFile(FILE *);
+void readFileFromRange(FILE*, int, int);
+void updateStudent(long, char *, student *);
+
+#endif // DATAFILE_H_
